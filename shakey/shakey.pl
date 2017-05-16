ab45b37465647e30bd85a915049d47ea3b515230
res13 <andreas.erb@gmx.ch>
2017-05-16 19:56:28 +0200

    bugfixes in recursive solver

--- shakey/shakey.pl
+++ shakey/shakey.pl
@@ -1,19 +1,3 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Shakey - initial state %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-room(a).
-room(b).
-room(c).
-room(corridor).
-
-%% connected(X, Y) -- ob Raum X mit Raum Y verbunden ist
-connected(a,corridor).
-connected(b,corridor).
-connected(c,corridor).
-
-%% inroom(X) -- ob shakey sich im raum X befindet
-inroom(a).
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Shakey - actions (strips) %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -24,19 +8,64 @@ action(
     [inroom(Y)],
     [inroom(X)]).
 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Shakey - recursive way to print a list %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+print_list(0, _) :- !.
+print_list(_, []).
+print_list(N, [H|T]) :-
+    write(H),
+    nl,
+    N1 is N - 1,
+    print(N1, T).
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Shakey - recursive forward solution searcher %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 solve(State, Goal, Plan, Plan):-
-    is_subset(Goal, State). %% check wheter the actual state is already the goal
+    subset(Goal, State), %% check wheter the goal is a subset of the actual state
+    write('finished!'),
+    print_list(5, Plan).
+
 solve(State, Goal, Sofar, Plan):-
     action(ActionName, Preconditions, DeleteList, AddList), %% calles the action
     not(member(ActionName, Sofar)), %% checks if this action was already called
-    is_subset(Preconditions, State), %% checks if the
-    delete_list(DeleteList, State, Remainder), %% deletes the given states from the current state list
+    subset(Preconditions, State), %% checks whether the precondition is a subset of the actual state
+    delete(DeleteList, State, Remainder), %% deletes the given states from the current state list
     append(AddList, Remainder, NewState), %% adds the given states to the current state list
     solve(NewState, Goal, [ActionName|Sofar], Plan). %% recursively calls itself with the updated values
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Shakey - runner with initial state and goal %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+run(S, E) :- solve(S, E, [S], []).
+
+
+%%%%%%%%%%%%%%%%%%%%
+%% Shakey - tests %%
+%%%%%%%%%%%%%%%%%%%%
+
+test1 :-
+    run(
+        [connected(a, corridor), connected(b, corridor), connected(c, corridor), inroom(a)],
+        [inroom(corridor)]). %% move from room a to corridor
+
+test2 :-
+    run(
+        [connected(a, corridor), connected(b, corridor), connected(c, corridor), inroom(a)],
+        [inroom(c)]). %% move from room a over corridor to room c
+
+
+
+
 
 
 
