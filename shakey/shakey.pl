c82c43b457a132059dab640aecd5ea61ee94fb48
res13 <andreas.erb@gmx.ch>
2017-05-19 09:44:47 +0200

    added door open/close actions

--- shakey/shakey.pl
+++ shakey/shakey.pl
@@ -1,3 +1,26 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Shakey - fixed states %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+add_fixed_states(X, Result) :-
+    append(X, [
+               connected(a, corridor),
+               connected(b, corridor),
+               connected(c, corridor)
+           ], Result).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Shakey - variable states %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+print_variable_states :-
+    write('Possible parameters (rooms) = a, b, c, corridor'), nl,
+    write('- inroom(X)'), nl,
+    write('- doorOpen(X, Y)'), nl.
+
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Shakey - actions (strips) %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -8,19 +31,28 @@ action(
     [inroom(Y)],
     [inroom(X)]).
 
+action(
+    openDoor(X,Y),
+    [connected(X,Y), not(doorOpen(X,Y)), (inroom(X); inroom(Y))],
+    [doorOpen(X,Y)],
+    []).
+
+action(
+    closeDoor(X,Y),
+    [connected(X,Y), doorOpen(X,Y), (inroom(X); inroom(Y))],
+    [],
+    [doorOpen(X,Y)]).
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Shakey - recursive way to print a list %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-print_list(0, _) :- !.
-print_list(_, []).
-print_list(N, [H|T]) :-
-    write(H),
-    nl,
-    N1 is N - 1,
-    print(N1, T).
+print_list([]).
+print_list([X|List]) :-
+    write(X),nl,
+    print_list(List).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -29,16 +61,16 @@ print_list(N, [H|T]) :-
 
 solve(State, Goal, _, Actions):-
     subset(Goal, State), %% check wheter the goal is a subset of the actual state
-    write('finished!'),
-    print_list(5, Actions).
+    write('finished with the following actions:'), nl,
+    print_list(Actions).
 
 solve(State, Goal, Sofar, Actions):-
-    action(ActionName, Preconditions, AddList, DeleteList), %% choose the right action
+    action(ActionName, Preconditions, AddList, DeleteList), %% choose next action
     subset(Preconditions, State), %% checks if the preconditions of this action are valid
     subtract(State, DeleteList, Remainder), %% deletes the given states from the current state list
     append(Remainder, AddList, NewState), %% adds the given states to the current state list
-    not(member(ActionName, Sofar)), %% checks if this action was already called
-    solve(NewState, Goal, [ActionName|Sofar], Actions), !. %% recursively calls itself with the updated values
+    not(member(NewState, Sofar)), %% checks if the new state was already reached (loop avoiding)
+    solve(NewState, Goal, [NewState|Sofar], [ActionName|Actions]), !. %% recursively calls itself with the updated values
 
 
 
@@ -46,7 +78,10 @@ solve(State, Goal, Sofar, Actions):-
 %% Shakey - runner with initial state and goal %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-run(S, E) :- solve(S, E, [S], []).
+run(S, E) :-
+    add_fixed_states(S, S2),
+    add_fixed_states(E, E2),
+    solve(S2, E2, [S2], []).
 
 
 %%%%%%%%%%%%%%%%%%%%
@@ -55,13 +90,13 @@ run(S, E) :- solve(S, E, [S], []).
 
 test1 :-
     run(
-        [connected(a, corridor), connected(b, corridor), connected(c, corridor), inroom(a)],
-        [connected(a, corridor), connected(b, corridor), connected(c, corridor), inroom(corridor)]). %% move from room a to corridor
+        [inroom(a)],
+        [inroom(corridor)]). %% move from room a to corridor
 
 test2 :-
     run(
-        [connected(a, corridor), connected(b, corridor), connected(c, corridor), inroom(a)],
-        [connected(a, corridor), connected(b, corridor), connected(c, corridor), inroom(c)]). %% move from room a over corridor to room c
+        [inroom(a)],
+        [inroom(c)]). %% move from room a over corridor to room c
 
 
 
