23f7dc73cd701b77071dd5e0f7d614c1a0b4b40a
res13 <andreas.erb@gmx.ch>
2017-05-30 20:58:47 +0200

    added things

--- shakey/shakey.pl
+++ shakey/shakey.pl
@@ -4,6 +4,13 @@
 
 add_fixed_states(X, Result) :-
     append(X, [
+		   room(room1),
+		   room(room2),
+		   room(room3),
+		   room(corridor),
+		   door(door1),
+		   door(door2),
+		   door(door3),
                connected(room1, corridor),
                connected(room2, corridor),
                connected(room3, corridor),
@@ -22,6 +29,12 @@ add_fixed_states(X, Result) :-
 
 shakey_help :-
     write('Fixed conditions:'), nl,
+    write('- room(room1)'), nl,
+    write('- room(room2)'), nl,
+    write('- room(room3)'), nl,
+    write('- door(door1)'), nl,
+    write('- door(door2)'), nl,
+    write('- door(door3)'), nl,
     write('- connected(room1, corridor)'), nl,
     write('- connected(room2, corridor)'), nl,
     write('- connected(room3, corridor)'), nl,
@@ -32,8 +45,9 @@ shakey_help :-
     write('- connected(corridor, door2)'), nl,
     write('- connected(corridor, door3)'), nl,
     write('Possible conditions to set:'), nl,
-    write('- in(Object, RoomName)'), nl,
-    write('- status(Object, StatusName)'), nl.
+    write('- in(RobotName / ObjectName, RoomName)'), nl,
+    write('- status(DoorName, StatusName)'), nl,
+    write('- object(ObjectName)'), nl.
 
 
 %%%%%%%%%%%%%%%%%%%%%%
@@ -42,37 +56,37 @@ shakey_help :-
 
 action(
     move(From, To),
-    [in(shakey, From), connected(From, To), connected(From, Door), connected(To, Door), status(Door, open)],
+    [room(From), room(To), in(shakey, From), connected(From, To), connected(From, Door), connected(To, Door), status(Door, open)],
     [in(shakey, To)],
     [in(shakey, From)]).
 
 action(
     move(From, To),
-    [in(shakey, From), connected(To, From), connected(From, Door), connected(To, Door), status(Door, open)],
+    [room(From), room(To), in(shakey, From), connected(To, From), connected(From, Door), connected(To, Door), status(Door, open)],
     [in(shakey, To)],
     [in(shakey, From)]).
 
 action(
-    grab(Object, RoomName),
-    [in(Object, RoomName), in(shakey, RoomName), in(nothing, shakeysHand)],
-    [in(Object, shakeysHand)],
-    [in(Object, RoomName), in(nothing, shakeysHand)]).
+    grab(ObjectName, RoomName),
+    [object(ObjectName), room(RoomName), in(ObjectName, RoomName), in(shakey, RoomName), in(nothing, shakeysHand)],
+    [in(ObjectName, shakeysHand)],
+    [in(ObjectName, RoomName), in(nothing, shakeysHand)]).
 
 action(
-    put(Object, RoomName),
-    [in(Object, shakeysHand), in(shakey, RoomName)],
-    [in(Object, RoomName), in(nothing, shakeysHand)],
-    [in(Object, shakeysHand)]).
+    put(ObjectName, RoomName),
+    [object(ObjectName), room(RoomName), in(ObjectName, shakeysHand), in(shakey, RoomName)],
+    [in(ObjectName, RoomName), in(nothing, shakeysHand)],
+    [in(ObjectName, shakeysHand)]).
 
 action(
     closeDoor(DoorName, RoomName),
-    [connected(RoomName, DoorName), status(DoorName, open), in(shakey, RoomName)],
+    [door(DoorName), room(RoomName), connected(RoomName, DoorName), status(DoorName, open), in(shakey, RoomName)],
     [status(DoorName, closed)],
     [status(DoorName, open)]).
 
 action(
     openDoor(DoorName, RoomName),
-    [connected(RoomName, DoorName), status(DoorName, closed), in(shakey, RoomName)],
+    [door(DoorName), room(RoomName), connected(RoomName, DoorName), status(DoorName, closed), in(shakey, RoomName)],
     [status(DoorName, open)],
     [status(DoorName, closed)]).
 
@@ -93,10 +107,16 @@ print_list_reversed([Head|Tail]) :-
 %% https://stackoverflow.com/questions/4912869/subsets-in-prolog %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+subset([], _).
 subset([Head|Tail], Set):-
     member(Head, Set),
     subset(Tail, Set).
-subset([], _).
+
+nestedListMember(List, [Head | Tail]) :-
+    permutation(List, Head);
+    nestedListMember(List, Tail).
+nestedListMember(List, [Last]) :-
+    permutation(List, Last).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -107,21 +127,24 @@ equal_lists(List1, List2) :-
 	subset(List1, List2),
 	subset(List2, List1).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Shakey - recursive depth-first solver %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-solve(State, Goal, Actions):-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Shakey - forward depth-first solver %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+solve(State, Goal, Sofar, Actions):-
     equal_lists(Goal, State), %% checks wheter the goal and the state are equal
+    print_list_reversed(Sofar),
     print_list_reversed(Actions).
 
-solve(State, Goal, Actions):-
+solve(State, Goal, Sofar, Actions):-
     action(ActionName, Preconditions, AddList, DeleteList), %% choose next action
     subset(Preconditions, State), %% checks if the preconditions of this action are valid
-    not(member(ActionName, Actions)), %% checks if this action was already called (loop avoiding)
+    not(member(ActionName, Actions)), %% checks if this action was already called (avoids loops)
     subtract(State, DeleteList, Remainder), %% deletes the given states from the current state list
     append(Remainder, AddList, NewState), %% adds the given states to the current state list
-    solve(NewState, Goal, [ActionName|Actions]), !. %% recursively calls itself with the updated values
+    not(nestedListMember(NewState, Sofar)), %% checks if this state was already reached (avoids unnecessary actions)
+    solve(NewState, Goal, [NewState|Sofar], [ActionName|Actions]), !. %% recursively calls itself with the updated values
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -131,7 +154,7 @@ solve(State, Goal, Actions):-
 run(Start, Goal) :-
     add_fixed_states(Start, FullStart),
     add_fixed_states(Goal, FullGoal),
-    solve(FullStart, FullGoal, []).
+    solve(FullStart, FullGoal, [FullStart], []).
 
 
 %%%%%%%%%%%%%%%%%%%%
@@ -165,20 +188,20 @@ test4 :-
 test5 :-
     write('take the box1 from room1 in the hand'), nl,
     run(
-        [in(shakey, room1), in(nothing, shakeysHand), in(box1, room1)],
-        [in(shakey, room1), in(box1, shakeysHand)]).
+        [in(shakey, room1), in(nothing, shakeysHand), in(box1, room1), object(box1)],
+        [in(shakey, room1), in(box1, shakeysHand), object(box1)]).
 
 test6 :-
     write('put the box1 from the hand in the room1'), nl,
     run(
-        [in(shakey, room1), in(box1, shakeysHand)],
-        [in(shakey, room1), in(nothing, shakeysHand), in(box1, room1)]).
+        [in(shakey, room1), in(box1, shakeysHand), object(box1)],
+        [in(shakey, room1), in(nothing, shakeysHand), in(box1, room1), object(box1)]).
 
 test7 :-
     write('take the box1 from room1, move to corridor and put the box1 in the corridor'), nl,
     run(
-        [in(shakey, room1), in(box1, room1), in(nothing, shakeysHand), status(door1, closed)],
-        [in(shakey, corridor), in(box1, corridor), in(nothing, shakeysHand), status(door1, open)]).
+        [in(shakey, room1), in(box1, room1), in(nothing, shakeysHand), status(door1, closed), object(box1)],
+        [in(shakey, corridor), in(box1, corridor), in(nothing, shakeysHand), status(door1, open), object(box1)]).
 
 test8 :-
     write('move from room1 over corridor to room3 with open doors'), nl,
@@ -187,10 +210,16 @@ test8 :-
         [in(shakey, room3), status(door1, open), status(door3, open)]).
 
 test9 :-
+    write('take the box1 from room1, move to room3 and put the box1 in the room3 (all doors cloesd and must be closed afterwards)'), nl,
+    run(
+        [in(shakey, room1), in(box1, room1), in(nothing, shakeysHand), status(door1, closed), status(door3, closed), object(box1)],
+        [in(shakey, room3), in(box1, room3), in(nothing, shakeysHand), status(door1, closed), status(door3, closed), object(box1)]).
+
+test10 :-
     write('take the box1 from room1, move to room3, put the box1 in room3, move to room2, take the box2 from room2, move to room1, put the box2 to room1 (all doors are closed and must be closed afterwards)'), nl,
     run(
-        [in(shakey, room1), in(nothing, shakeysHand), in(box1, room1), in(box2, room2), status(door1, closed), status(door2, closed), status(door3, closed)],
-        [in(shakey, room1), in(nothing, shakeysHand), in(box1, room3), in(box2, room1), status(door1, closed), status(door2, closed), status(door3, closed)]).
+        [in(shakey, room1), in(nothing, shakeysHand), in(box1, room1), in(box2, room2), status(door1, closed), status(door2, closed), status(door3, closed), object(box1), object(box2)],
+        [in(shakey, room1), in(nothing, shakeysHand), in(box1, room3), in(box2, room1), status(door1, closed), status(door2, closed), status(door3, closed), object(box1), object(box2)]).
 
 
 runAllTests :-
@@ -202,4 +231,5 @@ runAllTests :-
     test6,
     test7,
     test8,
-    test9.
+    test9,
+    test10.
